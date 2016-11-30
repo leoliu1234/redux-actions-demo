0ed9025acc2eb8a7e526ea1308bd3fead5a91ba7
leo---liu <leo---liu@hotmail.com>
2016-11-30 23:17:14 +0800

    fix issues for redux actions

--- src/actions/dashboard.js
+++ src/actions/dashboard.js
@@ -3,6 +3,6 @@ import { createActions } from 'redux-actions';
 // export const increment = createAction('INCREMENT', amount => amount);
 
 export const { increment, decrement } = createActions({
-  INCREMENT: amount => ({ amount }),
-  DECREMENT: amount => ({ amount })
+    INCREMENT: amount => amount,
+    DECREMENT: amount => amount
 });
