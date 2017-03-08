1f53ea97d61b370f9b83260db2adce208ca930c7
leo---liu <leo---liu@hotmail.com>
2017-03-08 22:39:28 +0800

    Add object rest spread transform

--- src/reducers/home.js
+++ src/reducers/home.js
@@ -2,10 +2,10 @@ import { handleActions } from 'redux-actions';
 
 const home = handleActions({
     INCREMENT: (state, action) => ({
-        counter: state.counter + action.payload
+        ...state, counter: state.counter + action.payload
     }),
     DECREMENT: (state, action) => ({
-        counter: state.counter - action.payload
+        ...state, counter: state.counter - action.payload
     })
 }, { counter: 0 });
 
