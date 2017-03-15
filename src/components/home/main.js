b8010c05e0185ecafc52b9f8eaa74e09e1f101f6
T04395 <Leo.Liu@augmentum.com.cn>
2017-03-15 20:45:11 +0800

    Center main section

--- src/components/home/main.js
+++ src/components/home/main.js
@@ -3,41 +3,44 @@ import React from 'react';
 export default class Main extends React.Component {
     render() {
         return (
-            <div id="main">
-                <table className="pure-table pure-table-bordered">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Make</th>
-                            <th>Model</th>
-                            <th>Year</th>
-                        </tr>
-                    </thead>
+            <main id="main">
+                <section id="content">
+                    <table className="pure-table pure-table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Make</th>
+                                <th>Model</th>
+                                <th>Year</th>
+                            </tr>
+                        </thead>
 
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Honda</td>
-                            <td>Accord</td>
-                            <td>2009</td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Honda</td>
+                                <td>Accord</td>
+                                <td>2009</td>
+                            </tr>
 
-                        <tr>
-                            <td>2</td>
-                            <td>Toyota</td>
-                            <td>Camry</td>
-                            <td>2012</td>
-                        </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Toyota</td>
+                                <td>Camry</td>
+                                <td>2012</td>
+                            </tr>
 
-                        <tr>
-                            <td>3</td>
-                            <td>Hyundai</td>
-                            <td>Elantra</td>
-                            <td>2010</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                            <tr>
+                                <td>3</td>
+                                <td>Hyundai</td>
+                                <td>Elantra</td>
+                                <td>2010</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section >
+
+            </main >
         );
     }
 }
