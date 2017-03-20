da30c195c6f7497945ec384d3f0d96a22c322b8b
T04395 <Leo.Liu@augmentum.com.cn>
2017-03-20 12:44:45 +0800

    Show Iamge

--- src/components/home/main.js
+++ src/components/home/main.js
@@ -2,47 +2,21 @@ import React, { PropTypes } from 'react';
 
 export default class Main extends React.Component {
     render() {
-        const {images} = this.props;
+        const { images } = this.props;
+
+        var imageElements = images.map(img => {
+            return <div className="pure-u-1-5">
+                <div className="image-details">
+                    <img src={img.url} />
+                </div>
+            </div>
+        });
         return (
             <div id="main">
                 <header className="header">Header</header>
                 <section className="content">
                     <div className="pure-g">
-                        <div className="pure-u-1-3">
-                            <div className="image-details">
-                                <img src="/images/src_1.jpg" />
-                            </div>
-                        </div>
-                        <div className="pure-u-1-3">
-                            <div className="image-details">
-                                <img src="/images/src_2.jpg" />
-                            </div>
-                        </div>
-                        <div className="pure-u-1-3">
-                            <div className="image-details">
-                                <img src="/images/src_3.jpg" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="pure-g">
-                        <div className="pure-u-1-3">{JSON.stringify(images)}</div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                    </div>
-                    <div className="pure-g">
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                    </div>
-                    <div className="pure-g">
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                    </div>
-                    <div className="pure-g">
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
-                        <div className="pure-u-1-3"><p>Thirds</p></div>
+                        {imageElements}
                     </div>
                 </section>
                 <footer className="footer">Footer</footer>
